ea99b913d087c12072adc2369bd34432c9dd88d2
Taichis Bogdan <116056308+BogdanKraxL@users.noreply.github.com>
2022-11-14 17:13:45 +0200

    Add files via upload

---
+++ Binary Clock/Binary Clock/Program.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Clock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dtn = DateTime.Now;
+            int ora, min, sec, aux;
+            string binary;
+
+            ora = dtn.Hour;
+            min = dtn.Minute;
+            sec = dtn.Second;
+            binary = Convert.ToString(ora / 10, 2);
+            aux = Convert.ToInt32(binary);
+
+            if (aux % 10 == 1)
+            {
+                Console.Write("X");
+            }
+            else
+            {
+                Console.Write("O");
+            }
+            aux /= 10;
+
+            if (aux % 10 == 1)
+            {
+                Console.Write("X");
+            }
+            else
+            {
+                Console.Write("O");
+            }
+
+            Console.WriteLine();
+            binary = Convert.ToString(ora % 10, 2);
+            aux = Convert.ToInt32(binary);
+
+            if (aux % 10 == 1)
+            {
+                Console.Write("X");
+            }
+            else
+            {
+                Console.Write("O");
+            }
+            aux /= 10;
+
+            if (aux % 10 == 1)
+            {
+                Console.Write("X");
+            }
+            else
+            {
+                Console.Write("O");
+            }
+            aux /= 10;
+
+            if (aux % 10 == 1)
+            {
+                Console.Write("X");
+            }
+            else
+            {
+                Console.Write("O");
+            }
+            aux /= 10;
+
+            if (aux % 10 == 1)
+            {
+                Console.Write("X");
+            }
+            else
+            {
+                Console.Write("O");
+            }
+            Console.WriteLine();
+
+            binary = Convert.ToString(min / 10, 2);
+            aux = Convert.ToInt32(binary);
+
+            if (aux % 10 == 1)
+            {
+                Console.Write("X");
+            }
+            else
+            {
+                Console.Write("O");
+            }
+            aux /= 10;
+
+            if (aux % 10 == 1)
+            {
+                Console.Write("X");
+            }
+            else
+            {
+                Console.Write("O");
+            }
+            aux /= 10;
+
+            if (aux % 10 == 1)
+            {
+                Console.Write("X");
+            }
+            else
+            {
+                Console.Write("O");
+            }
+            aux /= 10;
+            
+            Console.WriteLine();
+
+            binary = Convert.ToString(min % 10, 2);
+            aux = Convert.ToInt32(binary);
+
+            if (aux % 10 == 1)
+            {
+                Console.Write("X");
+            }
+            else
+            {
+                Console.Write("O");
+            }
+            aux /= 10;
+
+            if (aux % 10 == 1)
+            {
+                Console.Write("X");
+            }
+            else
+            {
+                Console.Write("O");
+            }
+            aux /= 10;
+
+            if (aux % 10 == 1)
+            {
+                Console.Write("X");
+            }
+            else
+            {
+                Console.Write("O");
+            }
+            aux /= 10;
+
+            if (aux % 10 == 1)
+            {
+                Console.Write("X");
+            }
+            else
+            {
+                Console.Write("O");
+            }
+            aux /= 10;
+
+            Console.WriteLine();
+
+            binary = Convert.ToString(sec / 10, 2);
+            aux = Convert.ToInt32(binary);
+
+            if (aux % 10 == 1)
+            {
+                Console.Write("X");
+            }
+            else
+            {
+                Console.Write("O");
+            }
+            aux /= 10;
+
+            if (aux % 10 == 1)
+            {
+                Console.Write("X");
+            }
+            else
+            {
+                Console.Write("O");
+            }
+            aux /= 10;
+
+            if (aux % 10 == 1)
+            {
+                Console.Write("X");
+            }
+            else
+            {
+                Console.Write("O");
+            }
+            aux /= 10;
+
+            Console.WriteLine();
+
+            binary = Convert.ToString(sec % 10, 2);
+            aux = Convert.ToInt32(binary);
+
+            if (aux % 10 == 1)
+            {
+                Console.Write("X");
+            }
+            else
+            {
+                Console.Write("O");
+            }
+            aux /= 10;
+
+            if (aux % 10 == 1)
+            {
+                Console.Write("X");
+            }
+            else
+            {
+                Console.Write("O");
+            }
+            aux /= 10;
+
+            if (aux % 10 == 1)
+            {
+                Console.Write("X");
+            }
+            else
+            {
+                Console.Write("O");
+            }
+            aux /= 10;
+
+            if (aux % 10 == 1)
+            {
+                Console.Write("X");
+            }
+            else
+            {
+                Console.Write("O");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
